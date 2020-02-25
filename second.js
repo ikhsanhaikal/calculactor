c0d3cd02b048a07a4c056555744ac844e42b630f
ikhsan haikal <andvelop4@gmail.com>
2020-02-25 23:10:02 +0700

    adding the logic for the calculactor

---
+++ second.js
@@ -0,0 +1,127 @@
+const display = document.querySelector('.display');
+const numbers = document.querySelectorAll('.numbers');
+const operators = document.querySelectorAll('.operators');
+let order = [], lval;
+let isReady = false;
+let hit = false;
+
+/* display */
+numbers.forEach(function(number){
+    number.addEventListener('click', ()=>{
+        if (isReady) {display.textContent = number.textContent; isReady = false; return;}
+        if (display.textContent === '0') {display.textContent = number.textContent; return;}
+        display.textContent += number.textContent;
+    })
+})
+
+/* display-2 
+   when you pressed operand button then press number, all previous number that was diplayed gone 
+*/
+operators.forEach(function(operator) {
+    operator.addEventListener('click', function() {
+        if(operator.textContent === '+') {
+            isReady = true;
+            order.push(display.textContent);
+            order.push(operator.textContent);
+            console.log(order);
+            if(order.length > 3) { 
+                operate(order[1], parseInt(order[0]), parseInt(order[2])); 
+            }
+        }
+        if(operator.textContent === '-') {
+            isReady = true;
+            order.push(display.textContent);
+            order.push(operator.textContent);
+            console.log(order);
+            if(order.length > 3) { 
+                operate(order[1], parseInt(order[0]), parseInt(order[2])); 
+            }
+        }
+        if(operator.textContent === '*') {
+            console.log('*');
+            isReady = true;
+            order.push(display.textContent);
+            order.push(operator.textContent);
+            console.log(order);
+            if(order.length > 3) { 
+                operate(order[1], parseInt(order[0]), parseInt(order[2])); 
+            }
+        }
+        if(operator.textContent === '/') {
+            console.log('/');
+            isReady = true;
+            order.push(display.textContent);
+            order.push(operator.textContent);
+            console.log(order);
+            if(order.length > 3) { 
+                operate(order[1], parseInt(order[0]), parseInt(order[2])); 
+            }
+        }
+        if(operator.textContent === '=') {
+            order.push(display.textContent);
+            console.log(order);
+            operate(order[1], parseInt(order[0]), parseInt(order[2]));
+            order.shift();
+        }
+    })
+})
+
+
+
+
+/*--------------------------------------------------*/
+
+function operate(operator, lval, rval) {
+    switch (operator) {
+        case '+':
+            result = adding(lval, rval);
+            order.shift();
+            order.shift();
+            display.textContent = result;
+            order[0] = display.textContent;
+            console.log('from operate',order);
+            break;
+        case '-':
+            result = substract(lval, rval);
+            order.shift();
+            order.shift();
+            display.textContent = result;
+            order[0] = display.textContent;
+            break;
+        case '*':
+            result = multiply(lval, rval);
+            order.shift();
+            order.shift();
+            display.textContent = result;
+            order[0] = display.textContent;
+            break;
+        case '/':
+            result = divide(lval, rval);
+            order.shift();
+            order.shift();
+            display.textContent = result;
+            order[0] = display.textContent;
+            break;
+        default:
+            console.log('operator is not defined');
+    }
+}
+
+/*--------------------------------------------------*/
+
+function adding(lval, rval) {
+    return lval + rval;
+}
+
+function substract(lval, rval) {
+    return lval - rval;
+}
+
+function multiply(lval, rval) {
+    return lval * rval;
+}
+
+function divide(lval, rval) {
+    return lval / rval;
+}
+
